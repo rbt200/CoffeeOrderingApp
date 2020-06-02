bda4eeda2907ee6815a1ee2f73e49dae3d6d4c11
rbt200 <myesoap@mail.ru>
2020-06-02 12:32:46 -0700

    Add CreateCoffeeOrder

---
+++ CoffeeOrderingApp/Models/CreateOrderResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CreateOrderResponse.swift
+//  CoffeeOrderingApp
+//
+//  Created by Ivan Ivanov on 02.06.2020.
+//  Copyright © 2020 Ivan Ivanov. All rights reserved.
+//
+
+import Foundation
+
+struct CreateOrderResponse: Decodable {
+    let success: Bool
+}
--- CoffeeOrderingApp/Services/Webservice.swift
+++ CoffeeOrderingApp/Services/Webservice.swift
@@ -8,6 +8,36 @@
 
 import Foundation
 
+func createCoffeeOrder(order: Order, completion: @escaping(CreateOrderResponse?) -> ()) {
+    
+    guard let url = URL(string: "https://actually-mire-infinity.glitch.me/orders") else {
+        fatalError("Invalid URL")
+    }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpBody = try? JSONEncoder().encode(order)
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        
+        guard let data = data, error == nil else {
+            DispatchQueue.main.async {
+                completion(nil)
+            }
+            return
+        }
+        
+        let createOrderResponse = try? JSONDecoder().decode(CreateOrderResponse.self, from: data)
+        DispatchQueue.main.async {
+            completion(createOrderResponse)
+        }
+        
+    }.resume()
+    
+}
+
+
 class Webservice {
     func getAllOrders(completion: @escaping ([Order]?) -> ()) {
         
